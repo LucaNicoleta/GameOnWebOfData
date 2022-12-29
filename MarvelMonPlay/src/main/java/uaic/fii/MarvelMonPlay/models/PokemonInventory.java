5d1740918f19bd1b8473c47e561ed4a229c5e6c7
Butnaru Gheorghita <butnaru.gheorghita@yahoo.com>
2022-12-29 20:42:38 +0200

    Added pokemon inventory model

---
+++ MarvelMonPlay/src/main/java/uaic/fii/MarvelMonPlay/models/PokemonInventory.java
@@ -0,0 +1,24 @@
+package uaic.fii.MarvelMonPlay.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PokemonInventory {
+    private List<Pokemon> pokemonInventory;
+
+    public PokemonInventory(){
+        pokemonInventory = new ArrayList<>();
+    }
+
+    public List<Pokemon> getPokemonInventory() {
+        return pokemonInventory;
+    }
+
+    public void addPokemonToInventory(Pokemon pokemon) {
+        pokemonInventory.add(pokemon);
+    }
+
+    public void removePokemonFromInventory(Pokemon pokemon){
+        pokemonInventory.remove(pokemon);
+    }
+}

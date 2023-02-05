83f399b5cfc893c8d9006a74781f9af3b0b8094d
hazicatalin <61392637+hazicatalin@users.noreply.github.com>
2023-02-05 16:32:16 +0200

    Update PlayerRepository.java

--- MarvelMonPlay/src/main/java/uaic/fii/MarvelMonPlay/repositories/PlayerRepository.java
+++ MarvelMonPlay/src/main/java/uaic/fii/MarvelMonPlay/repositories/PlayerRepository.java
@@ -7,4 +7,5 @@ public interface PlayerRepository {
     TupleQueryResult findPlayerByUsername(String username);
     void save(Player player, boolean cascadeSave);
     void delete(Player player);
+    void update(Player player, boolean b);
 }

39d30ff3d5b9ab453eacd9e246e6bae49d45e159
Butnaru Gheorghita <butnaru.gheorghita@yahoo.com>
2023-02-12 15:40:42 +0200

    Login & Register Update

--- ui/src/login/login.js
+++ ui/src/login/login.js
@@ -14,10 +14,13 @@ export default function Login() {
   const [username, setUsername] = React.useState('');
   const [password, setPassword] = React.useState('');
   const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleSetUsername = (event) => setUsername(event.target.value);
+  const handleSetPassword = (event) => setPassword(event.target.value);
 
   const handleMouseDownPassword = (event) => {
     event.preventDefault();
   };
+
   return (
     <Box sx={{ backgroundImage: `url(${'https://ambicular.com/skuawk-download/space/patrick-mcmanaman.jpg'})`, backgroundRepeat: 'no-repeat', backgroundSize: '100% 100%' }} >
       <Grid container direction={'column'} rowGap={0.2} justifyContent='space-around' alignItems={'space-around'}>
@@ -30,8 +33,6 @@ export default function Login() {
             <Grid item xs={3} display={'flex'} justifyContent='start'>
               <Box className='square' bgcolor='#f0fe2f' sx={{ borderColor: '#f0fe2f' }}></Box>
             </Grid>
-
-
           </Grid>
 
         </Grid>
@@ -49,8 +50,8 @@ export default function Login() {
                     <FormControl sx={{ m: 1, width: '25ch', marginLeft: '16px', marginTop: '16px', }} variant="standard">
                       <InputLabel sx={{ color: 'brown', fontWeight: 'bolder' }} htmlFor="username-register" >Username</InputLabel>
                       <Input
-                        // value = "username"
-                        onChange={() => setUsername(React.event.target.value)}
+                        // username input
+                        onChange={handleSetUsername}
                         id="username-register"
                         type='text'
                       />
@@ -60,11 +61,11 @@ export default function Login() {
                       <Input
                         id="standard-adornment-password"
                         type={showPassword ? 'text' : 'password'}
+                        // password input
+                        onChange={handleSetPassword}
                         endAdornment={
                           <InputAdornment position="end">
-                            <IconButton
-                              // value = "password"
-                              onChange={() => setPassword(React.event.target.value)}
+                            <IconButton 
                               aria-label="toggle password visibility"
                               onClick={handleClickShowPassword}
                               onMouseDown={handleMouseDownPassword}
@@ -79,8 +80,10 @@ export default function Login() {
                   <Box sx={{ marginLeft: '12px', paddingLeft: '2%', backgroundImage: `url(${'./logo_game.png'})`, backgroundSize: '100% 100%', width: '50%', margin: 'auto', height: '100%' }}></Box>
                 </Box>
 
-                <Button type='submit' onClick={() => action === "LOGIN" ? doLogin(username, password) : doRegister(username, password)} href='/play' variant='contained' sx={{ bgcolor: 'brown', width: '40%', alignSelf: 'end', marginRight: '16px', marginTop: '28px' }}>{action === "LOGIN" ? "Login" : "Register"}</Button>
+                <Button type='submit' onClick={() => action === "LOGIN" ? doLogin(username, password) : doRegister(username, password)} variant='contained' sx={{ bgcolor: 'brown', width: '40%', alignSelf: 'end', marginRight: '16px', marginTop: '28px' }}>{action === "LOGIN" ? "Login" : "Register"}</Button>
+                {/* href='/play' */}
               </Box>
+              {/*  */}
             </Grid>
             <Grid item xs={3} display={'flex'} justifyContent='start'>
               <Box className='login-tab-text' sx={{
@@ -115,38 +118,47 @@ export default function Login() {
   );
 }
 
-function doLogin(username, password){
+function doLogin (username, password) {
   const xhr = new XMLHttpRequest();
-  xhr.open("POST", "localhost:8080/auth/signup");
+  xhr.open("POST", "http://localhost:8080/login");
   xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
   var data = new FormData();
   data.append('username', username);
   data.append('password', password);
+
+  console.log("username: " + username);
+  console.log("pass: " + password);
+
   xhr.onload = function(){
-    if (xhr.readyState === 4 && xhr.status === 201) {
-      console.log(JSON.parse(xhr.responseText));
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      console.log(xhr.responseText);
+      //redirect("/play");
     } else {
-      console.log(`Error: ${xhr.status}`);
+      console.log("Error: " + xhr.response);
     }
   };
-  xhr.send(data);
-  
+  xhr.send(JSON.stringify({
+    username: username,
+    password: password
+  }));
+
 }
 
 function doRegister(username, password){
   const xhr = new XMLHttpRequest();
   xhr.open("POST", "http://localhost:8080/auth/signup");
   xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+  
   const body = JSON.stringify({
     username: username,
-    password: password,
-    RES_IDENTIFIER: username
+    password: password
   });
+
   xhr.onload = () => {
     if (xhr.readyState === 4 && xhr.status === 201) {
-      console.log(JSON.parse(xhr.responseText));
+      console.log(xhr.responseText);
     } else {
-      console.log(`Error: ${xhr.status}`);
+      console.log("Error: " + xhr.response)
     }
   };
   xhr.send(body);
